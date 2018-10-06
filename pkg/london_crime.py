7fdeeefae002b12ae73b781df0e0833700e3e100
MisaOgura <misa.ogura01@gmail.com>
2018-10-06 22:01:56 +0100

    Add docstring and docopt to parse sys arguments

--- pkg/london_crime.py
+++ pkg/london_crime.py
@@ -1,4 +1,45 @@
+"""LondonCrime
+
+Given a CSV file with longtitude and latitude fields, it accumulates the
+row count per coordinate (long, lat) and returns a GeoJSON which can be
+used as a data source for Mapbox APIs.
+
+Usage:
+    london_crime.py <input> [-o=<output>]
+    london_crime.py (-h | --help)
+
+Options:
+    <input>             Inout csv file with longtitude and latitude fields.
+    -o=<output>         Output file [default: output.geojson].
+    -h --help           Show this screen.
+"""
+
+import sys
+import json
 import pandas as pd
+from docopt import docopt
+
+
+# ----------------------------------------------------------------------------
+# Module level methods
+
+
+def main():
+    arguments = docopt(__doc__, version='0.1')
+
+    input_file = arguments['<input>']
+    output_file = arguments['-o']
+
+    london_crime = LondonCrime(input_file)
+
+    data = london_crime.to_geojson()
+
+    with open(output_file, 'w') as output:
+        json.dump(data, output)
+
+
+# ----------------------------------------------------------------------------
+# LondonCrime class
 
 
 class LondonCrime():
@@ -10,6 +51,7 @@ class LondonCrime():
         self._crime_freq_sr = self._df.groupby('Coordinate').size()
 
     def to_geojson(self):
+        # TODO - use https://github.com/frewsxcv/python-geojson
         geojson = {
             "type": "FeatureCollection",
             "features": []
@@ -37,3 +79,6 @@ class LondonCrime():
     def max_freq(self):
         return self._crime_freq_sr.max()
 
+
+if __name__ == '__main__':
+    main()
